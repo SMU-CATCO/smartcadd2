4df4855546698f1aa8188782ab135d2edb0a0e2a
Eli J Laird <ejlaird@slogin-01.superpod.smu.edu>
2024-10-21 14:28:48 -0500

    added shuffling and QM(X) dataset loaders for QM9 and QM40

--- experiments/datasets/jax_datasets.py
+++ experiments/datasets/jax_datasets.py
@@ -2,18 +2,23 @@ from torch_geometric.data import Dataset
 import jax
 import jraph
 import jax.numpy as jnp
-
+import numpy as np
 
 # Inspired by https://github.com/google-deepmind/jraph/blob/master/jraph/ogb_examples/train.py
 class PyG2JraphDataset:
     def __init__(
-        self, pyg_dataset, batch_size: int = 1, dynamically_batch: bool = False
+        self, pyg_dataset, batch_size: int = 1, dynamically_batch: bool = False, shuffle: bool = False
     ):
         self.pyg_dataset = pyg_dataset
         self.batch_size = batch_size
         self.dynamically_batch = dynamically_batch
+        self.shuffle = shuffle
         self.max_num_nodes = max(data.num_nodes for data in pyg_dataset)
         self.max_num_edges = max(data.num_edges for data in pyg_dataset)
+        self._indices = np.arange(len(pyg_dataset))
+        if self.shuffle:
+            np.random.shuffle(self._indices)
+        self._current_index = 0
 
         if dynamically_batch:
             self.generator = jraph.dynamically_batch(
@@ -28,16 +33,30 @@ class PyG2JraphDataset:
     def __len__(self):
         return len(self.pyg_dataset)
 
+    def __iter__(self):
+        self._current_index = 0
+        if self.shuffle:
+            np.random.shuffle(self._indices)
+        return self
+
     def __next__(self):
+        if self._current_index >= len(self._indices):
+            raise StopIteration
+
         if self.dynamically_batch:
             batch = next(self.generator)
-            return self._pad_graph_to_nearest_power_of_two(batch)
+            self._current_index += self.batch_size
         else:
             batch = []
             for _ in range(self.batch_size):
-                batch.append(next(self.generator))
-            batched_graphs = jraph.batch(batch)
-            return self._pad_graph_to_nearest_power_of_two(batched_graphs)
+                if self._current_index >= len(self._indices):
+                    break
+                idx = self._indices[self._current_index]
+                batch.append(self.get_graph_at_idx(idx))
+                self._current_index += 1
+            batch = jraph.batch(batch)
+
+        return self._pad_graph_to_nearest_power_of_two(batch)
 
     def get_graph_at_idx(self, idx):
         pyg_data = self.pyg_dataset[idx]
@@ -105,4 +124,40 @@ class PyG2JraphDataset:
         pad_graphs_to = graphs_tuple.n_node.shape[0] + 1
         return jraph.pad_with_graphs(
             graphs_tuple, pad_nodes_to, pad_edges_to, pad_graphs_to
+        )
+
+
+class QMXJraphDataset(PyG2JraphDataset):
+    def __init__(self, pyg_dataset, batch_size: int = 1, dynamically_batch: bool = False, shuffle: bool = False):
+        super().__init__(pyg_dataset, batch_size, dynamically_batch, shuffle)
+
+    def get_graph_at_idx(self, idx):
+        pyg_data = self.pyg_dataset[idx]
+        node_features = {
+            "x": jnp.array(pyg_data.x),
+            "z": jnp.array(pyg_data.z),
+            "pos": jnp.array(pyg_data.pos),
+        }
+        edge_features = {
+            "edge_attr": jnp.array(pyg_data.edge_attr),
+        }
+        if hasattr(pyg_data, "edge_attr2"):
+            edge_features["edge_attr2"] = jnp.array(pyg_data.edge_attr2)
+        
+        global_features = {
+            "y": jnp.array(pyg_data.y),
+        }
+        if hasattr(pyg_data, "name"):
+            global_features["name"] = np.array([pyg_data.name])
+        if hasattr(pyg_data, "smiles"):
+            global_features["smiles"] = np.array([pyg_data.smiles])
+
+        return jraph.GraphsTuple(
+            nodes=node_features,
+            edges=edge_features,
+            senders=jnp.array(pyg_data.edge_index[0]),
+            receivers=jnp.array(pyg_data.edge_index[1]),
+            n_node=jnp.array([pyg_data.num_nodes]),
+            n_edge=jnp.array([pyg_data.num_edges]),
+            globals=global_features,
         )
